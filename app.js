842082ea39f88a1701fac44c48d390c4d901e68a
sahandbabali <sahand.babali@gmail.com>
2021-12-12 21:59:42 +0330

    after handlebars

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
 const app = express();
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
 const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/about", (req, res) => res.render("about"));
 // custom 404 page
 app.use((req, res) => {
-  res.type("text/plain");
   res.status(404);
-  res.send("404 - Not Found");
+  res.render("404");
 });
 // custom 500 page
 app.use((err, req, res, next) => {
   console.error(err.message);
-  res.type("text/plain");
   res.status(500);
-  res.send("500 - Server Error");
+  res.render("500");
 });
 app.listen(port, () =>
   console.log(
