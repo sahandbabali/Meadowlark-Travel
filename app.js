db768d2fee36ae64045d834628357ffa58ee56f0
sahandbabali <sahand.babali@gmail.com>
2021-12-12 22:22:27 +0330

    end of chapter 3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const expressHandlebars = require("express-handlebars");
 const app = express();
 app.engine(
   "handlebars",
-  expressHandlebars({
+  expressHandlebars.engine({
     defaultLayout: "main",
   })
 );
@@ -11,8 +11,23 @@ app.set("view engine", "handlebars");
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => res.render("home"));
-app.get("/about", (req, res) => res.render("about"));
+
+app.get("/about", (req, res) => {
+  const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+  ];
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+
+  res.render("about", { fortune: randomFortune });
+});
+
 // custom 404 page
 app.use((req, res) => {
   res.status(404);
